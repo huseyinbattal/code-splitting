fbd03890d54b4ceb1d8a472e480bc4e134850c0f
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-28 14:30:09 +0300

    chunk

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <button>Topla</button>
+        <button
+          onClick={() => {
+            import("./utils").then((x) => {
+              console.log(x.add(50, 100));
+            });
+          }}
+        >
+          Topla
+        </button>
+        <button
+          onClick={() => {
+            import("./converToUpper").then((x) => {
+              console.log(x.convertToUpper("emir"));
+            });
+          }}
+        >
+          Büyüt
+        </button>
       </header>
     </div>
   );
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,3 @@
 export function add(s1, s2) {
     return s1+s2
 }
-
-export function convertToUpper(str) {
-    return str.toUpperCase()
-}
